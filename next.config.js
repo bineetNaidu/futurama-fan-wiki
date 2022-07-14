c17cf0e4c240a935470705b128cff70d042b270c
bineetNaidu <bineet.s.naidu@gmail.com>
2022-07-14 14:36:20 +0530

    ✨ update next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['vignette.wikia.nocookie.net', 'res.cloudinary.com'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
