43c9f47b251d5c0bea5258d02cde4d45749b9084
bineetNaidu <bineet.s.naidu@gmail.com>
2022-07-14 21:26:56 +0530

    ♠️ add types defs

---
+++ lib/types.ts
@@ -0,0 +1,12 @@
+export type Character = {
+  Age: string;
+  FirstAppearance: string;
+  Name: string;
+  PicUrl: string;
+  Planet: string;
+  Profession: string;
+  Relatives: string;
+  Species: string;
+  Status: string;
+  VoicedBy: string;
+};
--- services/getAllCharacters.ts
+++ services/getAllCharacters.ts
@@ -1,19 +1,7 @@
 import { AxiosError } from 'axios';
 import useSwr from 'swr';
 import { fetcher } from '../lib/axiosClient';
-
-interface Character {
-  Age: string;
-  FirstAppearance: string;
-  Name: string;
-  PicUrl: string;
-  Planet: string;
-  Profession: string;
-  Relatives: string;
-  Species: string;
-  Status: string;
-  VoicedBy: string;
-}
+import type { Character } from '../lib/types';
 
 export const useGetAllCharacters = () => {
   const { data, error } = useSwr<Character[], AxiosError<{ message: string }>>(
